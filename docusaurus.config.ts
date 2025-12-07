fb7b999ba9d9dd7d5dd4dca4e2ce74e54e80d98f
Nimrahbutt <160144242+Nimrahbutt@users.noreply.github.com>
2025-12-08 01:17:44 +0500

    Fix GitHub Pages deployment config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://nimrahbutt.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Hackathon-1-physical-AI-humanoid-robotics/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Nimrahbutt', // Usually your GitHub org/user name.
+  projectName: 'Hackathon-1-physical-AI-humanoid-robotics', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
